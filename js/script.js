99f1248f9963b80ca0def9240f15cace9b49329c
valyafrankova22 <vfrankova22@icloud.com>
2021-10-05 20:51:47 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let operation = prompt( `Введите номер математической операции: 1)сложение, 2)вычитание, 3)умножение, 4)деление?`);
-let num1 = prompt(`Введите первое число`);
-let num2 = prompt(`Введите второе число`);
+const operation = prompt( `Введите номер математической операции: 1)сложение, 2)вычитание, 3)умножение, 4)деление?`);
+const num1 = Number( prompt(`Введите первое число`) ) ;
+const num2 = Number( prompt(`Введите второе число`) ) ;
 
 if (operation === `1`) alert(num1 + num2);
 if (operation === `2`) alert(num1 - num2);
